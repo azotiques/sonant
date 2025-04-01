057018a74e0f02c143dc15f9709a41a0d061e567
Eduard Dohot <edidohot8@gmail.com>
2025-04-01 03:16:50 +0300

    New one 2

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import { redirect } from "next/navigation";
 import LandingPage from "./_components/LandingPage";
 import Navigation from "./_components/Navigation";
 
 export default function Home() {
+  redirect("/signin");
+
   return <div>Not yet</div>;
 }
